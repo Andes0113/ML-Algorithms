42a6f1027e5f447736de64d855b51fd644ee6547
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2022-09-27 21:01:14 -0400

    K Nearest Neighbors

--- KNN/test_KNN.py
+++ KNN/test_KNN.py
@@ -2,14 +2,13 @@ from ctypes.wintypes import tagRECT
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import ListedColormap
 from KNN import KNN
 
 cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
-iris = dataset.load_iris()
-X, y = iris.data, iris.tagRECT
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1344)
 
 clf = KNN(k=5)
